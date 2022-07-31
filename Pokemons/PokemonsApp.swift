53cc54797780cc21a05664a70a09ee3e6f1670aa
ergeakova <ergeak@gmail.com>
2022-07-31 22:25:46 +0300

    Initial Commit

---
+++ Pokemons/PokemonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonsApp.swift
+//  Pokemons
+//
+//  Created by Erge Gevher Akova on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PokemonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
